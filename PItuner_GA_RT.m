7cf7088faff0b2cd3ab3c205e911c16b7c79dc01
jaimie swartz <jaimiexs@gmail.com>
2019-05-01 13:25:25 -0700

    finished GA codes

--- PItuner_GA_RT.m
+++ PItuner_GA_RT.m
@@ -1,18 +1,18 @@
 function kset = PItuner_GA_RT(sens,tau,dt,settleMax,OSmax)
-% note: running ga func requires global opt toolbox and opt toolbox
+    popsize =7; % # candidates per generation
+    MaxGenerations = 3; % # generations
+    s = tf('s');
+    %G = 1/(s*(s*s+s+1)); % plant you're controlling
+    G = sens/(tau*s+1); % constant, no dynamics
+    N=10; % horizon
+    rng(1,'twister') % control random number generation, for reproducibility
+    startPop=[-10; 10]; % bounds of starting params
+    % use gaoptimset instead of optimoptions, weird error occurs
+    options = gaoptimset('PopulationSize',popsize,'Generations',MaxGenerations,'PopInitRange',startPop);
+    plot=false; % dont plot while tuning
+    [kset,fval,exitflag,output,population,scores] = ga(@(K)pidtest_RT(G,dt,K,N,settleMax,OSmax,plot),2,-eye(2),zeros(2,1),[],[],[],[],[],options);
 
-popsize = 7; % # candidates per generation
-MaxGenerations = 5; % # generations
-s = tf('s');
-%G = 1/(s*(s*s+s+1)); % plant you're controlling
-G = sens/(tau*s+1); % constant, no dynamics
-N=10; % horizon
-
-rng(1,'twister') % control random number generation, for reproducibility
-startPop=[-10; 10]; % bounds of starting params
-options = optimoptions(@ga,'PopulationSize',popsize,'MaxGenerations',MaxGenerations,'PopInitRange',startPop);
-[kset,fval,exitflag,output,population,scores] = ga(@(K)pidtest_RT(G,dt,K,N,settleMax,OSmax),2,-eye(2),zeros(2,1),[],[],[],[],[],options);
-
-close all
-disp('--------------------------------------');
-pidtest(G,dt,kset,N); title('Best step resp');
+    disp('-------- Best -----------------------------');
+    plot=true; % plot best controller
+    pidtest_RT(G,dt,kset,N,settleMax,OSmax,plot); title(strcat('PItuner: CL step resp, BEST kset=',num2str(kset)));
+end
--- computeK_way3.m
+++ computeK_way3.m
@@ -19,10 +19,10 @@ function [Kp_vmag,Ki_vmag,Kp_vang,Ki_vang,Vmag_ctrlStart,Vang_ctrlStart]=...
         OSmax=[0.2 0.1]; % vmag vang
 
         for i=1:length(dvdq) %for each actuator-phase, det kset={kp ki}
-            kset = PItuner_GA_RT(dvdq(i),tau,Ts,settleMax(1),OSmax(1)) % Q-V loop
+            kset = PItuner_GA_RT(dvdq(i),tau,Ts,settleMax(1),OSmax(1)); % Q-V loop
             Kp_vmag(i)=kset(1); Ki_vmag=kset(2);
-            kset = PItuner_GA_RT(ddeldp(i),tau,Ts,settleMax(2),OSmax(2)) % P-del loop
-            Kp_vang(i)=kset(1); Ki_vang=kset(2);
+             kset = PItuner_GA_RT(ddeldp(i),tau,Ts,settleMax(2),OSmax(2)); % P-del loop
+             Kp_vang(i)=kset(1); Ki_vang=kset(2);
         end   
      end
     % Kp_vmag and the others are each rx1 vectors
